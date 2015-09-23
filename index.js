759748e308070b95b2166d5859a2f2ab6eea8d94
mrWh1te <mlage@lagetech.com>
2015-09-23 17:28:27 -0500

    added support for html and interpolate expressions such as those found in ngMessageFormat

--- index.js
+++ index.js
@@ -24,88 +24,172 @@ var PLUGIN_NAME = 'gulp-pseudo-translate-angular-json';
 var config = {
   increasePercent: 30
 };
+var openMode = false; // flag for angularProtect
 
 
 // Translate Line & Check For Padding Config
 function pseudoLine(translatedLine) {
-  var pseudoTranslatedLine = pseudoWord(translatedLine);
+  var pseudoTranslatedLine = pseudoWords(translatedLine);
 
   var extraLength = Math.round(translatedLine.length * config.increasePercent / 100.0);
 
   var extraWords = " lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget urna laoreet, accumsan felis at, dapibus elit. In ut tempus mauris. Sed eget sagittis arcu, in condimentum purus. Curabitur vitae congue elit.";
 
-  pseudoTranslatedLine += pseudoWord(extraWords.substr(0, extraLength));
+  pseudoTranslatedLine += pseudoWords(extraWords.substr(0, extraLength));
 
   return pseudoTranslatedLine;
 }
 
-// Actual Translation. Goes through string, char by char,
-// replacing regular letters with pseudo versions
-function pseudoWord(before) {
-  var after = '';
 
-  for (var i = 0; i < before.length; i++) {
-    var c = before.charAt(i);
-    var out = '';
-    switch (c) {
-      case 'a': out = 'á'; break;
-      case 'b': out = 'β'; break;
-      case 'c': out = 'ç'; break;
-      case 'd': out = 'δ'; break;
-      case 'e': out = 'è'; break;
-      case 'f': out = 'ƒ'; break;
-      case 'g': out = 'ϱ'; break;
-      case 'h': out = 'λ'; break;
-      case 'i': out = 'ï'; break;
-      case 'j': out = 'J'; break;
-      case 'k': out = 'ƙ'; break;
-      case 'l': out = 'ℓ'; break;
-      case 'm': out = '₥'; break;
-      case 'n': out = 'ñ'; break;
-      case 'o': out = 'ô'; break;
-      case 'p': out = 'ƥ'; break;
-      case 'q': out = '9'; break;
-      case 'r': out = 'ř'; break;
-      case 's': out = 'ƨ'; break;
-      case 't': out = 'ƭ'; break;
-      case 'u': out = 'ú'; break;
-      case 'v': out = 'Ʋ'; break;
-      case 'w': out = 'ω'; break;
-      case 'x': out = 'ж'; break;
-      case 'y': out = '¥'; break;
-      case 'z': out = 'ƺ'; break;
-      case 'A': out = 'Â'; break;
-      case 'B': out = 'ß'; break;
-      case 'C': out = 'Ç'; break;
-      case 'D': out = 'Ð'; break;
-      case 'E': out = 'É'; break;
-      case 'F': out = 'F'; break;
-      case 'G': out = 'G'; break;
-      case 'H': out = 'H'; break;
-      case 'I': out = 'Ì'; break;
-      case 'J': out = 'J'; break;
-      case 'K': out = 'K'; break;
-      case 'L': out = '£'; break;
-      case 'M': out = 'M'; break;
-      case 'N': out = 'N'; break;
-      case 'O': out = 'Ó'; break;
-      case 'P': out = 'Þ'; break;
-      case 'Q': out = 'Q'; break;
-      case 'R': out = 'R'; break;
-      case 'S': out = '§'; break;
-      case 'T': out = 'T'; break;
-      case 'U': out = 'Û'; break;
-      case 'V': out = 'V'; break;
-      case 'W': out = 'W'; break;
-      case 'X': out = 'X'; break;
-      case 'Y': out = 'Ý'; break;
-      case 'Z': out = 'Z'; break;
-      default: out = c; break;
+function angularProtect(i, text) {
+  /**
+   * Stop pseudoWords from destroying these:
+   *
+   * {{ SettingsCtrl.ProfileService.profile.mismatchedTravelNames.length }}
+   * {{trophiesCount, select, 1{Trophy} other{Trophies}}} (only touch the plualized forms...!)
+   * <a href=\"/web/agreement\" target=\"_blank\">User Agreement</a> (dont touch stuff inside < >)
+   */
+
+  // Take a look at the next character, how important is it, to the context of the moment ?
+  if (openMode) {
+    // look for closing tags
+    switch(text.charAt(i)) {
+      case '>':
+        openMode = false;
+
+      break;
+      case '{':
+        openMode = ! checkForInterpolateExpression(i, text, false);
+
+      break;
     }
-    after += out;
+  } else {
+    // look for opening tags
+    switch(text.charAt(i)) {
+      case '<':
+        openMode = true;
+
+      break;
+      case '{':
+        openMode = checkForInterpolateExpression(i, text, true);
+
+      break;
+    }
+  }
+}
+
+/**
+ * [checkForInterpolateExpression description]
+ * @param  {[type]} i     [description]
+ * @param  {[type]} text  [description]
+ * @param  {[type]} start [description]
+ * @return {[type]}       [description]
+ * @description
+ *   Checks the next character in the text. Depending on 'start', it will look for either the
+ *   end or beginning of an interpolate expression, {{ var }}. The idea is we want to protect the inside
+ *   of these expression so they evaluate normamly in javascript. Hence the need to maintain openMode
+ *
+ *   When openMode is true, we are in an Interpolate expression that must not be converted into pseudo
+ */
+function checkForInterpolateExpression(i, text, start) {
+  var checkFor = start ? '{' : '}';
+
+  if (text.charAt(i+1) === checkFor)
+  {
+    return true;
   }
 
-  return after;
+  return false;
+}
+
+function pseudoLetter(c) {
+    switch (c) {
+      case 'a': c = 'á'; break;
+      case 'b': c = 'β'; break;
+      case 'c': c = 'ç'; break;
+      case 'd': c = 'δ'; break;
+      case 'e': c = 'è'; break;
+      case 'f': c = 'ƒ'; break;
+      case 'g': c = 'ϱ'; break;
+      case 'h': c = 'λ'; break;
+      case 'i': c = 'ï'; break;
+      case 'j': c = 'J'; break;
+      case 'k': c = 'ƙ'; break;
+      case 'l': c = 'ℓ'; break;
+      case 'm': c = '₥'; break;
+      case 'n': c = 'ñ'; break;
+      case 'o': c = 'ô'; break;
+      case 'p': c = 'ƥ'; break;
+      case 'q': c = '9'; break;
+      case 'r': c = 'ř'; break;
+      case 's': c = 'ƨ'; break;
+      case 't': c = 'ƭ'; break;
+      case 'u': c = 'ú'; break;
+      case 'v': c = 'Ʋ'; break;
+      case 'w': c = 'ω'; break;
+      case 'x': c = 'ж'; break;
+      case 'y': c = '¥'; break;
+      case 'z': c = 'ƺ'; break;
+      case 'A': c = 'Â'; break;
+      case 'B': c = 'ß'; break;
+      case 'C': c = 'Ç'; break;
+      case 'D': c = 'Ð'; break;
+      case 'E': c = 'É'; break;
+      case 'F': c = 'F'; break;
+      case 'G': c = 'G'; break;
+      case 'H': c = 'H'; break;
+      case 'I': c = 'Ì'; break;
+      case 'J': c = 'J'; break;
+      case 'K': c = 'K'; break;
+      case 'L': c = '£'; break;
+      case 'M': c = 'M'; break;
+      case 'N': c = 'N'; break;
+      case 'O': c = 'Ó'; break;
+      case 'P': c = 'Þ'; break;
+      case 'Q': c = 'Q'; break;
+      case 'R': c = 'R'; break;
+      case 'S': c = '§'; break;
+      case 'T': c = 'T'; break;
+      case 'U': c = 'Û'; break;
+      case 'V': c = 'V'; break;
+      case 'W': c = 'W'; break;
+      case 'X': c = 'X'; break;
+      case 'Y': c = 'Ý'; break;
+      case 'Z': c = 'Z'; break;
+    }
+
+    return c;
+}
+
+// Actual Translation. Goes through string, char by char,
+// replacing regular letters with pseudo versions
+function pseudoWords(text) {
+  var translated = '';
+
+  // need support for :
+
+  /**
+   * {{ SettingsCtrl.ProfileService.profile.mismatchedTravelNames.length }}
+   * {{trophiesCount, select, 1{Trophy} other{Trophies}}} (only touch the plualized forms...!)
+   * <a href=\"/web/agreement\" target=\"_blank\">User Agreement</a> (dont touch stuff inside < >)
+   */
+
+  for (var i = 0; i < text.length; i++) {
+    // Look for: {{ }} < >
+    // To set or unset openMode
+    angularProtect(i, text);
+
+    if (openMode) {
+      translated += text.charAt(i);
+    } else {
+      translated += pseudoLetter(text.charAt(i));
+    }
+  }
+
+  // Reset mode for next string
+  openMode = false;
+
+  return translated;
 }
 
 // Plugin level function(dealing with files)
